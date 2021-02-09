15d52f27312c06624a73974f4c8cc125b7239dbe
Liang Ma <maliang@51hitech.com>
2021-02-09 14:16:37 +0800

    1. Don't use weighted box fusion and use correct parameters got from the paper. 2. Resize images to normalize vehicle pixel sizes. 3. Save detection results of batch detection on a file folder. 4. Update notes.

--- infer.py
+++ infer.py
@@ -68,7 +68,7 @@ def make_predictions(images, net, score_threshold=0.22):
             })
     return [predictions]
 
-def run_wbf(predictions, image_index, image_size, iou_thr=0.01, skip_box_thr=0.009, weights=None):
+def run_wbf(predictions, image_index, image_size, iou_thr=0.55, skip_box_thr=0.1, weights=None):
     boxes = [(prediction[image_index]['boxes']/(image_size-1)).tolist() for prediction in predictions]
     scores = [prediction[image_index]['scores'].tolist() for prediction in predictions]
     labels = [prediction[image_index]['classes'].tolist() for prediction in predictions]
@@ -208,7 +208,7 @@ class DatasetRetriever(Dataset):
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-def predict(net, config, angle = 0.0, img_path = None, img_bytes = None, np_img = None, delay = 1):
+def predict(net, config, angle = 0.0, img_path = None, img_bytes = None, np_img = None, delay = 1, want_aabb = False):
     dataset = DatasetRetriever(crop_size=config.crop_size, overlap_size=config.overlap_size,
         image_scale=config.image_scale, path=img_path, img_bytes=img_bytes, np_img=np_img,
         transform=get_valid_transforms(config.image_scale), angle=angle)
@@ -225,7 +225,12 @@ def predict(net, config, angle = 0.0, img_path = None, img_bytes = None, np_img 
         predictions = make_predictions(images, net)
         for i, img in enumerate(images):
             img = img.permute(1,2,0).cpu().numpy()
-            boxes, scores, labels = run_wbf(predictions, image_index=i, image_size=config.image_scale)
+            if 1:
+                boxes = predictions[0][i]['boxes']
+                scores = predictions[0][i]['scores']
+                labels = predictions[0][i]['classes']
+            else:
+                boxes, scores, labels = run_wbf(predictions, image_index=i, image_size=config.image_scale)
             for k, label in enumerate(labels):
                 #bbox = boxes[k]
                 bbox = boxes[k].tolist()
@@ -243,8 +248,7 @@ def predict(net, config, angle = 0.0, img_path = None, img_bytes = None, np_img 
                     polygon = polygon.tolist()
                     polygon = [[round(p[0],4), round(p[1],4)] for p in polygon]
                     obj = {"label":float(label), "score":float(scores[k]), "polygon":polygon}
-                    if config.show_img:
-                        obj["crop_idx"] = crop_idx[i]
+                    if want_aabb or config.show_img:
                         obj["box"] = box
                     results.append(obj)
                 img = cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0,0,255), 3)
@@ -266,24 +270,18 @@ def predict(net, config, angle = 0.0, img_path = None, img_bytes = None, np_img 
             class_id = int(obj["label"])
             start_point = (int(bbox[0]), int(bbox[1])) 
             end_point = (int(bbox[2]), int(bbox[3])) 
-            c = color[obj["crop_idx"]%len(color)] if config.box_color is None else config.box_color
-            del obj["crop_idx"]
-            del obj["box"]
-            if 1:
-                c = (1, 0, 0)           #    red: (0.8, 1.0)
-                if score < 0.5:         #  green: (0.6, 0.8)
-                    c = (1, 1, 0)       # yellow: (0.0, 0.6)
-                if score < 0.7:
-                    c = (0, 1, 0)
-            if 1:
-                img2 = cv2.rectangle(img2, start_point, end_point, c, 2)
-            else:
-                contour = np.array(polygon)
-                img2 = cv2.drawContours(img2, [np.int0(contour)], 0, c, 2)
-            class_name_maps = ["bg", "car"]
+            if not want_aabb:
+                del obj["box"]
+            c = (1, 0, 0)           #    red: (0.7, 1.0)
+            if score < 0.5:         #  green: (0.5, 0.7)
+                c = (1, 1, 0)       # yellow: (0.0, 0.5)
+            if score < 0.7:
+                c = (0, 1, 0)
+            img2 = cv2.rectangle(img2, start_point, end_point, c, 2)
+            #class_name_maps = ["bg", "car"]
             #class_name_maps = ["bg", "ped", "bike", "motor"]
             #print(class_id)
-            img2 = cv2.putText(img2, class_name_maps[class_id], start_point, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2, cv2.LINE_AA) 
+            #img2 = cv2.putText(img2, class_name_maps[class_id], start_point, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2, cv2.LINE_AA) 
 
         img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
         if config.save_img:
@@ -319,7 +317,7 @@ def predict_obb(net, config, img_path = None, img_bytes = None, np_img = None, d
     print(len(objs))
     return objs
 
-def run(path, angles):
+def run(path, angles, common_vehicle_length=None):
     net, config = init_net()
 
     if len(angles) == 0:
@@ -331,6 +329,7 @@ def run(path, angles):
         output_base_path = path + "_det"
         if not os.path.isdir(output_base_path):
             os.mkdir(output_base_path)
+        config.save_img = True
         for item in os.listdir(path):
             ext = item[item.rfind(".")+1:]
             if ext.lower() not in ("png", "jpg", "jpeg", "bmp"):
@@ -338,8 +337,16 @@ def run(path, angles):
             start = time.time()
             img_path = os.path.join(path, item)
             output_path = os.path.join(output_base_path, item)
-            config.img_name = output_path[0:output_path.rfind(".")]+".framed.jpg"
-            results = predict(net, config, angle=0.0, img_path=img_path)
+            config.img_name = output_path[0:output_path.rfind(".")]+config.img_suffix+".jpg"
+            if common_vehicle_length is not None:
+                attrs_json_path = img_path[0:img_path.rfind(".")]+".video_attrs.json"
+                with open(attrs_json_path) as f:
+                    attrs = json.load(f)
+                config.crop_size = int(768.0 * attrs["MostCommonVehicleLengthInPixels"] / common_vehicle_length)
+                config.overlap_size = int(config.crop_size * 200.0 / 768.0) // 2 * 2
+                print("crop: %d/%d" % (config.overlap_size, config.crop_size))
+            config.img_name = output_path[0:output_path.rfind(".")]+".jpg"
+            results = predict(net, config, angle=0.0, img_path=img_path, want_aabb=True, delay=1)
             txt_path = output_path[0:output_path.rfind(".")]+".txt" if config.save_result else None
             if txt_path is not None:
                 with open(txt_path, "w") as f:
@@ -400,12 +407,14 @@ def run(path, angles):
 def init_net():
     class Config:
         box_color = None
-        show_img = False
+        show_img = True
         save_img = False
         img_name = "save_16.png"
+        img_suffix = ""
         save_result = True
         result_format = "json"
         #result_format = "txt"
+
     config = Config()
 
     config.box_color = (0,0,255)
@@ -462,12 +471,16 @@ def init_net():
         
     return net, config
 
-if __name__ == '__main__':
+def main():
     if torch.cuda.device_count() > 1:
         torch.cuda.set_device(0)
         print("Select [%s]" % torch.cuda.get_device_name(torch.cuda.current_device()))
 
-    if len(sys.argv) < 2:
-        print("Usage: python infer.py test.jpg [0 30 60]")
+    print(sys.argv)
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Usage: python infer.py test.jpg [60.0]")
     else:
-        run(sys.argv[1], sys.argv[2:])
+        run(sys.argv[1], [], common_vehicle_length=(float(sys.argv[2]) if len(sys.argv) > 2 else None))
+
+if __name__ == '__main__':
+    main()
